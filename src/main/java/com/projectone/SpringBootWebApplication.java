bf613888b89acd4b09ba1cab497243de36e9ce73
subhash <subash90.nkl@gmail.com>
2018-06-13 21:41:24 +0530

    first commit

--- src/main/java/com/projectone/SpringBootWebApplication.java
+++ src/main/java/com/projectone/SpringBootWebApplication.java
@@ -2,13 +2,20 @@ package com.projectone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 //http://www.thymeleaf.org/doc/articles/layouts.html
 @SpringBootApplication
-public class SpringBootWebApplication {
+public class SpringBootWebApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SpringBootWebApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootWebApplication.class);
+    }
 
 }
